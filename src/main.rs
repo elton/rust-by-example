f32d00864a10ebf2a62200cbec72aee9256df810
Elton Zheng <elton.zheng.sh@gmail.com>
2020-09-03 14:06:14 +0800

    try ownership.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2020 Elton Zheng
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+mod owership;
+
 fn main() {
-    println!("Hello, world!");
+    owership::main();
 }
