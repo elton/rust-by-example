ebc63d6abb85ac946de48174c25cab5f44594cc5
Elton Zheng <elton.zheng.sh@gmail.com>
2020-09-03 14:19:28 +0800

    try borrowing.

---
+++ src/immutable.rs
@@ -0,0 +1,32 @@
+// Copyright 2020 Elton Zheng
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub fn main() {
+    let immutable_box = Box::new(5u32);
+
+    println!("immutable_box contains {}", immutable_box);
+
+    // 可变性错误
+    //*immutable_box = 4;
+
+    // *移动* box，改变所有权（和可变性）
+    let mut mutable_box = immutable_box;
+
+    println!("mutable_box contains {}", mutable_box);
+
+    // 修改 box 的内容
+    *mutable_box = 4;
+
+    println!("mutable_box now contains {}", mutable_box);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-mod owership;
+
+// mod owership;
+// mod raii;
+// mod immutable;
+mod borrowing;
 
 fn main() {
-    owership::main();
+    // owership::main();
+    // raii::main();
+    // immutable::main();
+    borrowing::main();
 }
