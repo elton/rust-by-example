1ac3f4af7000a826151f221b73d198ea75aa4700
Elton Zheng <elton.zheng.sh@gmail.com>
2020-09-06 12:30:07 +0800

    try string

--- src/strings.rs
+++ src/strings.rs
@@ -6,4 +6,7 @@ pub fn main() {
     println!("the len of {} is {}", s, s.len());
     let hello = "中文测试";
     println!("{} the first 3 character are: {}", hello, &hello[0..3]);
+    for b in hello.bytes() {
+        print!("{},", b);
+    }
 }
