f60974015e4306132f4a88ccb4bd935782c2b130
Elton Zheng <elton.zheng.sh@gmail.com>
2020-09-10 17:49:48 +0800

    add some iterator test.

--- src/iter.rs
+++ src/iter.rs
@@ -39,4 +39,14 @@ pub fn main() {
     let new_x = x.iter().cloned().take(2).collect::<Vec<_>>(); // Rust 1.1之后推出了新的Cloned方法，解决了这个问题。
 
     println!("the new x is: {:?}", new_x);
+
+    let _ = (0..5)
+        // .inspect(|x| println!("before flat_map: {}", x))
+        .flat_map(|x| x * 100..x * 110)
+        // .inspect(|x| println!("after flat_map: {}", x))
+        .enumerate()
+        // .inspect(|(i, v)| println!("index:{}, value:{}", i, v))
+        .filter(|&(i, x)| (i + x) % 3 == 0)
+        // .collect::<Vec<(_, _)>>();
+        .for_each(|(i, x)| println!("{}:{}", i, x));
 }
